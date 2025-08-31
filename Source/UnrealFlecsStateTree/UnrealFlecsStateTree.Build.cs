fbe9350ef381e9be0f18e0c9d25ee9f98e8f5db0
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2025-08-31 19:22:21 -0400

    Unreal Initial Commit

---
+++ Source/UnrealFlecsStateTree/UnrealFlecsStateTree.Build.cs
@@ -0,0 +1,42 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class UnrealFlecsStateTree : ModuleRules
+{
+	public UnrealFlecsStateTree(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				ModuleDirectory,
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// ... add other private include paths required here ...
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				// ... add other public dependencies that you statically link with here ...
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+			}
+			);
+	}
+}
